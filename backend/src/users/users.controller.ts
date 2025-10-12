73412e4b6a5377fd8a133cea3ed3b9f16c16b69e
y0hannes <yohanneset3@gmail.com>
2025-10-12 18:41:55 +0300

    feat(users): Create CRUD endpoints using class-validator

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,17 +1,69 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getAll() {
+  async getAll() {
     return this.usersService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
-    return this.usersService.getOne(id);
+  async getOne(@Param('id') id: string) {
+    const user = await this.usersService.getOne(id);
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    try {
+      return this.usersService.create(createUserDto);
+    } catch (error) {
+      throw new HttpException(
+        'User creation failed: ' + error.message,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      return await this.usersService.update(id, updateUserDto);
+    } catch (error) {
+      throw new HttpException(
+        'User update failed: ' + error.message,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    try {
+      return await this.usersService.remove(id);
+    } catch (error) {
+      throw new HttpException(
+        'User deletion failed: ' + error.message,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
   getAll() {
-    return {
-      msg: 'all the users will be returned',
-    };
+    return this.prisma.user.findMany();
   }
 
   getOne(id: string) {
-    return {
-      msg: `Only a single user with id ${id} will be returned`,
-    };
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      // data: {
+      //   name: createUserDto.name,
+      //   email: createUserDto.email,
+      //   password: createUserDto.password,
+      // },
+      data: createUserDto,
+    });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
