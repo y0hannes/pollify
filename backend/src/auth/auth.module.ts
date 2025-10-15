3eaf2a3470d6d6114bc705def42cefecbaeb9df4
y0hannes <yohanneset3@gmail.com>
2025-10-15 10:14:19 +0300

    Create jwt strategy for validating signed in user

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [UsersModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
